0bae31564588b8a0c0365dde91d3e125225ec887
Wes Carroll <wcarroll@users.noreply.github.com>
2023-03-18 01:35:33 +0000

    Initial Commit

---
+++ runaisdk/main.py
@@ -0,0 +1,23 @@
+import httpx
+
+
+class RunClient:
+    _users = None
+    _projects = None
+    _accessToken = None
+
+    def __init__(
+        self,
+        appId: str,
+        appSecret: str,
+        realm: str,
+        base_url: str = "https://app.run.ai",
+    ) -> None:
+        self._client = httpx.Client(base_url=base_url)
+        self._appId = appId
+        self._appSecret = appSecret
+        self._realm = realm
+
+
+def main():
+    RunClient("api", "1234541", "testing")
